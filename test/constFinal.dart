ce23310a531e1884f9f463c499550d7a568ec274
0x1m4o <chuntdevnx@gmail.com>
2023-02-14 13:21:10 +0700

    Const and Final

---
+++ test/constFinal.dart
@@ -0,0 +1,16 @@
+void main() {
+  /// Both const and final are not replaceable. But const value must initialized at the same time the variables created.
+  /// Const
+  // const nama = 'Chandra';
+
+  /// Final
+  // final nama;
+  // nama = 'Chandra';
+  // OR
+  // final nama = 'Chandra';
+
+  /// Try to replace (error)
+  // nama = 'Dodi';
+
+  // print(nama);
+}
