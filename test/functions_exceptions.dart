e9783c9f8e0ea31e8eb5e945d5de08dbf87e5c72
0x1m4o <chuntdevnx@gmail.com>
2023-02-14 10:30:05 +0700

    Functions and Exceptions

--- test/functions_exceptions.dart
+++ test/functions_exceptions.dart
@@ -10,8 +10,11 @@ void main() {
 /*   var notNullFunc = penjumlahanNotNull(data1: 20, data2: 30);
   print(notNullFunc); */
 
-  var hasil = pembagian(4, 0);
-  print(hasil);
+/*   var notNullFuncPos = penjumlahanPositionNotNull(20);
+  print(notNullFuncPos); */
+
+/*   var hasil = pembagian(4, 0);
+  print(hasil); */
 }
 
 /// Position Operators
@@ -25,6 +28,23 @@ void main() {
 /// The parameters position is not that that matters as long the value is not null.
 /* int penjumlahanName({data1, data2, data3}) {
   return data1 + data2 + data3;
+} */
+
+/// More advanced position function.
+/// [] is to specify the optional arguments.
+/* int penjumlahanPositionNotNull(data1, [data2, data3]) {
+  var hasil;
+  if (data3 != null) {
+    hasil = data1 + data2 + data3;
+  } else if (data2 != null) {
+    hasil = data1 + data2;
+  } else if (data1 != null) {
+    hasil = data1;
+  } else {
+    hasil = 0;
+  }
+
+  return hasil;
 } */
 
 /// More advanced name function.
@@ -44,8 +64,10 @@ void main() {
   return hasil;
 } */
 
+
+
 /* Try Catch */
-pembagian(data1, data2) {
+/* pembagian(data1, data2) {
   try {
     return data1 ~/ data2; // Always return the value to int (similiar to floor)
   } catch (err) {
@@ -53,4 +75,4 @@ pembagian(data1, data2) {
   } finally {
     print('This is still run');
   }
-}
+} */
