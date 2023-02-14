ca33b95a43bef7bcbfaa70bd00fdde6df3df53f9
0x1m4o <chuntdevnx@gmail.com>
2023-02-14 15:08:31 +0700

    looping using for, while, and do-while

---
+++ test/looping.dart
@@ -0,0 +1,86 @@
+void main() {
+  /// For Loop
+  /** Looping
+   *  Inside for we set the rules for the loops
+   *  1 Arguments for the start of the loop.
+   *  2 Arguments for the end of the loop.
+   *  3 Arguments for the loop process.
+   */
+
+  /// Increment +1
+  // for (int i = 1; i <= 10; i++) {
+  //   print(i);
+  // }
+
+  /// Increment +n
+  // for (int i = 1; i <= 100; i += 5) {
+  //   print(i);
+  // }
+
+  /// Decrement -1
+  // for (int i = 10; i >= 1; i--) {
+  //   print(i);
+  // }
+
+  /// Decrement -n
+  // for (int i = 100; i >= 1; i -= 5) {
+  //   print(i);
+  // }
+
+  /// While Loop
+  // int i = 1;
+  // while (i <= 10) {
+  //   print(i);
+  //   i++;
+  // }
+
+  /// Do - While Loop
+  // int i = 1;
+  // do {
+  //   print(i);
+  //   i++;
+  // } while (i <= 10);
+
+  /// Infinite Loop
+  // int i = 1;
+  // while (i <= 10) {
+  //   print(i);
+  // }
+
+  /// Ordering Arrays
+  /// Ascending
+  // List data = [1, 4, 3, 5, 0, 2, 6];
+  // int res;
+  // for (int index = 0; index < data.length; index++) {
+  //   // Logic
+  //   for (int sort = index + 1; sort < data.length; sort++) {
+  //     if (data[index] > data[sort]) {
+  //       res = data[index];
+  //       data[index] = data[sort];
+  //       data[sort] = res;
+  //     }
+  //   }
+  // }
+
+  // for (int i = 0; i < data.length; i++) {
+  //   print(data[i]);
+  // }
+
+  /// Descending
+  // List data = [1, 4, 3, 5, 0, 2, 6];
+  // int res;
+  // for (int index = 0; index < data.length; index++) {
+  //   // Logic
+  //   for (int sort = index + 1; sort < data.length; sort++) {
+  //     if (data[index] > data[sort]) {
+  //       res = data[index];
+  //       data[index] = data[sort];
+  //       data[sort] = res;
+  //     }
+  //   }
+  // }
+
+  // for (int i = 0; i < data.length; i++) {
+  //   print(data[i]);
+  // }
+}
