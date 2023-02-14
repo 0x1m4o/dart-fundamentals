6cf494154583f7aa53febca7fe8758f46022345c
0x1m4o <chuntdevnx@gmail.com>
2023-02-14 13:28:22 +0700

    Null Safety

---
+++ test/nullSafety.dart
@@ -0,0 +1,11 @@
+void main() {
+  /// Null Safety is the method that can you perform to prevent the variable value to be null
+  // String? nama;
+  // print(nama!);
+
+  /// Late
+  /// late used to create a empty variables but you need to specify the data in the future.
+  // late String eat;
+  // eat = "at 09.00";
+  // print(eat);
+}
