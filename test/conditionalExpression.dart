99ebe3d14ed7b4df1c11b16e231c25bf342eeb52
0x1m4o <chuntdevnx@gmail.com>
2023-02-14 21:21:24 +0700

    Conditional Expression

---
+++ test/conditionalExpression.dart
@@ -0,0 +1,48 @@
+void main() {
+  /** Conditional Expression
+   */
+
+  /// If Else
+  // int grade = 80;
+  // if (grade >= 0 && grade < 50) {
+  //   print('Tidak Lulus');
+  // } else if (grade >= 50 && grade <= 75) {
+  //   print('Bagus');
+  // } else if (grade > 75 && grade < 100) {
+  //   print('Sangat Bagus');
+  // } else if (grade == 100) {
+  //   print('Sempurna');
+  // } else {
+  //   print("Angka yang dimasukkan tidak valid");
+  // }
+
+  /// Single line Conditional Expression
+  // int grade = 80;
+  // grade < 50 ? print('Tidak Lulus') : print("Lulus");
+
+  /// Switch Case
+  // int nilaiA = 10;
+  // int nilaiB = 5;
+
+  // String operator = "/";
+  // switch (operator) {
+  //   case "+":
+  //     print(nilaiA + nilaiB);
+  //     break;
+  //   case "-":
+  //     print(nilaiA - nilaiB);
+  //     break;
+  //   case "/":
+  //     print(nilaiA / nilaiB);
+  //     break;
+  //   case "*":
+  //     print(nilaiA * nilaiB);
+  //     break;
+  //   case "%":
+  //     print(nilaiA % nilaiB);
+  //     break;
+  //   default:
+  //     print('Operator Salah');
+  //     break;
+  // }
+}
